565c59e24899d82f6608b54c9877d7135b8e82ed
Josue Makuta <makutajosue@gmail.com>
2022-06-29 14:20:58 +0200

    Calculator Refactored

--- app.js
+++ app.js
@@ -1,85 +1,39 @@
 
-
-
-
 const myButtons = document.querySelectorAll('button')
 const myInput = document.querySelector('input')
 
-function clear() {
-   myInput.value = "";
+
+myButtons.forEach(printToScreen)
+
+function printToScreen(elt, index) {
+   elt.addEventListener("click", function () {
+      if (index == 0) {
+         myInput.value = "";
+      } else if (index == 1) {
+         myInput.value = myInput.value.slice(0, -1);
+      } else if (index == 2) {
+         myInput.value = myInput.value / 100;
+      } else if (index == 16) {
+         let myVal = myInput.value.split('');
+         console.log(myVal)
+         if (myVal[0] != '-') {
+            myVal.unshift('-');
+         } else {
+            myVal.shift();
+         }
+         myInput.value = myVal.join('');
+      } else if (index == 19) {
+         while (myInput.value[0] == 0 && /[1-9]/.test(myInput.value[1])) {
+            myInput.value = myInput.value.slice(1)
+         }
+         myInput.value = (eval(myInput.value))
+      } else if (index == 7) {
+         myInput.value += "*";
+      } else {
+         myInput.value += elt.innerText;
+      }
+   })
 }
 
-myButtons[0].addEventListener('click', function () {
-   myInput.value = "";
-})
 
-myButtons[1].addEventListener('click', function () {
-   myInput.value = myInput.value.slice(0, -1);
-})
 
-myButtons[2].addEventListener('click', function () {
-   myInput.value = myInput.value / 100;
-})
-
-myButtons[3].addEventListener('click', function () {
-   myInput.value += "/";
-})
-
-myButtons[4].addEventListener('click', function () {
-   myInput.value += 7;
-})
-
-myButtons[5].addEventListener('click', function () {
-   myInput.value += 8;
-})
-myButtons[6].addEventListener('click', function () {
-   myInput.value += 9;
-})
-myButtons[7].addEventListener('click', function () {
-   myInput.value += "*";
-})
-myButtons[8].addEventListener('click', function () {
-   myInput.value += 4;
-})
-myButtons[9].addEventListener('click', function () {
-   myInput.value += 5;
-})
-myButtons[10].addEventListener('click', function () {
-   myInput.value += 6;
-})
-myButtons[11].addEventListener('click', function () {
-   myInput.value += "-";
-})
-myButtons[12].addEventListener('click', function () {
-   myInput.value += 1;
-})
-myButtons[13].addEventListener('click', function () {
-   myInput.value += 2;
-})
-myButtons[14].addEventListener('click', function () {
-   myInput.value += 3;
-})
-myButtons[15].addEventListener('click', function () {
-   myInput.value += '+';
-})
-myButtons[16].addEventListener('click', function () {
-
-   let myVal = myInput.value.split('');
-   console.log(myVal)
-   if (myVal[0] != '-') {
-      myVal.unshift('-');
-   } else {
-      myVal.shift();
-   }
-   myInput.value = myVal.join('');
-})
-myButtons[17].addEventListener('click', function () {
-   myInput.value += 0;
-})
-myButtons[18].addEventListener('click', function () {
-   myInput.value += '.';
-})
-
-myButtons[19].addEventListener('click', function () {
-   myInput.value = (eval(myInput.value))
-})
